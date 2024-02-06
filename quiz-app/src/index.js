bb36b988c3b6c1d3dba14c43b09019a34740734c
Helen Kuswik <hkuswik@web.de>
2024-02-06 17:04:08 +0100

    deployment with gh-pages should work now

--- quiz-app/src/index.js
+++ quiz-app/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import BrowserRouter from 'react-router-dom/BrowserRouter';
 
-/* const root = ReactDOM.createRoot(document.getElementById('root')); */
-ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}> {/* url changes based on dev mode */}
-        <App />
-    </BrowserRouter>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
 );
