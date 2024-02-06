70f1a4966a4d710b0cfd552bad1a008d533288dc
Helen Kuswik <hkuswik@web.de>
2024-02-06 16:54:09 +0100

    maybe now...?

--- quiz-app/src/index.js
+++ quiz-app/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import BrowserRouter from 'react-router-dom/BrowserRouter';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+/* const root = ReactDOM.createRoot(document.getElementById('root')); */
+ReactDOM.render(
     <BrowserRouter basename={process.env.PUBLIC_URL}> {/* url changes based on dev mode */}
         <App />
     </BrowserRouter>
